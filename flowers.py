77821a56ac7cb593f942e272867b423167e000e7
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-11-03 22:59:52 -0600

    girasol

--- flowers.py
+++ flowers.py
@@ -10,4 +10,6 @@ def menuPrincipal():
     op = 0
     while(op < 1 or op > 6): 
         print("Selecciona la opcion del menu")
-        print("1. Girasol\n2. Lavanda")
+        print("1. Girasol\n2. Lavanda\n3. Rosa\n4.Tulipan\n5.Ramo\n6.Salir")
+        if(op == 1):
+            print("Usted ha seleccionado un girasol")
--- girasol.py
+++ girasol.py
@@ -1,8 +1,30 @@
 import turtle as t
-
+t.speed(20)
 def petalo1():
-    t.color("yellow")
-    t.fillcolor("yellow")
+    t.color("#ffec24")
+    t.fillcolor("#f7ff69")
+    t.width(3)
+    t.pendown()
+    t.begin_fill()
+    t.circle(50, 100)
+    t.left(80)
+    t.circle(50, 100)
+    t.end_fill()
+    
+def petalo2():
+    t.color("#f8ff24")
+    t.fillcolor("#f8ff24")
+    t.width(3)
+    t.pendown()
+    t.begin_fill()
+    t.circle(80, 100)
+    t.left(80)
+    t.circle(80, 100)
+    t.end_fill()
+
+def petalo3():
+    t.color("#ffec24")
+    t.fillcolor("#ffec24")
     t.width(3)
     t.pendown()
     t.begin_fill()
@@ -11,4 +33,15 @@ def petalo1():
     t.circle(100, 100)
     t.end_fill()
 
-petalo1()
+for i in range(0,13):
+    t.right(30)
+    petalo3()
+
+for i in range(0,13):
+    t.right(30)
+    petalo2()
+
+for i in range(0,13):
+    t.right(30)
+    petalo1()
+
